97a2daa304652f3bd0875e541ba250df41f14bac
Ethan <ethanbaird94@gmail.com>
2022-07-24 14:30:01 +0100

    written tests for highest card

--- part_2_code/specs/card_game_tests.py
+++ part_2_code/specs/card_game_tests.py
@@ -16,3 +16,17 @@ class TestCardGame(unittest.TestCase):
     def test_can_check_for_ace__card_not_ace(self):
         is_ace = CardGame.check_for_ace(self.card2)
         self.assertEqual(False, is_ace)
+
+    def test_highest_card__card_1_highest(self):
+        highest = CardGame.highest_card(self.card2, self.card1)
+        self.assertEqual(self.card2, highest)
+
+    def test_highest_card__card_2_highest(self):
+        highest = CardGame.highest_card(self.card1, self.card2)
+        self.assertEqual(self.card2, highest)
+
+    def test_highest_card__cards_equal(self):
+        highest = CardGame.highest_card(self.card1, self.card1)
+        self.assertEqual('These cards have the same value!', highest)
+
+    
