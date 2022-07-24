0214be95e8ad989ee2966932981743a02b5ae9cc
Ethan <ethanbaird94@gmail.com>
2022-07-24 14:26:14 +0100

    written tests for check ace

--- part_2_code/specs/card_game_tests.py
+++ part_2_code/specs/card_game_tests.py
@@ -2,5 +2,17 @@ import unittest
 from src.card import Card
 from src.card_game import CardGame
 
-class TestCardGame:
-    pass
+class TestCardGame(unittest.TestCase):
+    
+    def setUp(self):
+        self.card1 = Card('hearts', 1)
+        self.card2 = Card('clubs', 7)
+        self.cards = [self.card1, self.card2]
+
+    def test_can_check_for_ace__card_is_ace(self):
+        is_ace = CardGame.check_for_ace(self.card1)
+        self.assertEqual(True, is_ace)
+
+    def test_can_check_for_ace__card_not_ace(self):
+        is_ace = CardGame.check_for_ace(self.card2)
+        self.assertEqual(False, is_ace)
